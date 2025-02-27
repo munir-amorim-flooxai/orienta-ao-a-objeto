f1ac630c97e4505b2645682b1f75125a57641627
munir-amorim-flooxai <munir.amorim.flooxai@gmail.com>
2025-02-27 14:00:20 -0300

    feat: enable decimal point functionality in calculator

--- calculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
+++ calculadora/curso-javascript-projeto-calculadora-clone-master/scripts/controller/CalcController.js
@@ -43,6 +43,8 @@ class CalcController {
     clearAll(){
 
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = '';
 
         this.setLastNumberToDisplay();
 
@@ -176,11 +178,7 @@ class CalcController {
 
                 this.setLastOperation(value);
 
-            } else if(isNaN(value)) {
-
-                console.log('Outra coisa', value);
-
-            }else{
+            } else{
 
                 this.pushOperation(value);
 
@@ -197,7 +195,7 @@ class CalcController {
             } else {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue));
+                this.setLastOperation(parseFloat(newValue));
 
                 this.setLastNumberToDisplay();
 
@@ -210,6 +208,20 @@ class CalcController {
     setError(){
 
         this.displayCalc = "Error";
+
+    }
+
+    addDot(){
+
+        let lastOperation = this.getLastOperation();
+
+        if (this.isOperator(lastOperation) || !lastOperation) {
+            this.pushOperation('0.');
+        }else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -243,8 +255,9 @@ class CalcController {
                     break;
                     
                 case 'ponto':
+                    this.addDot();
                     break;
-                    this.addOperation('.');
+                    
                 case '0':
                 case '1':
                 case '2':
